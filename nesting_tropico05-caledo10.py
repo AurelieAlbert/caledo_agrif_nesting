22e75e5aebc765e31a4b5164c11a948c11b8db94
alberta <alberta@ige-template-domOK.lthe.local>
2020-09-15 14:20:10 +0200

    up

---
+++ nesting_tropico05-caledo10.py
@@ -0,0 +1,15 @@
+#all the steps to create the nested files from TROPICO05 to CALEDO10
+
+#NESTING TOOLS executable
+
+agrif_coord=
+agrif_bathy=
+agrif_restart=
+
+
+#TROPICO05 files
+
+parent_coord=
+parent_bathy=
+
+
