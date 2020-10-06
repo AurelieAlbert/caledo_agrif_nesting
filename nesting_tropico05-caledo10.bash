dbe6e4daab157c8689a216cc06b5f42ee81daa08
auraoupa <auraoupa@gmail.com>
2020-10-06 10:33:51 +0200

    add script bash

---
+++ nesting_tropico05-caledo10.bash
@@ -0,0 +1,99 @@
+#all the steps to create the nested files from TROPICO05 to CALEDO10
+
+#Parameters
+
+parent=TROPICO05
+child=CALEDO10
+TMPDIR=/mnt/meom/workdir/alberta/CALEDO/NESTING_${parent}-${child}
+mkdir -p $TMPDIR
+
+latmin=-26.6
+latmax=-16.6
+lonmin=161.6
+lonmax=172
+
+#TROPICO05 files
+
+parent_coord=/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/coordinates_TROPICO05.nc
+parent_mesh=/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/mesh_mask_TROPICO05_L31_r4.0.3.nc
+parent_bathy_meter=/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/bathy_meter_TROPICO05.nc
+parent_domain=/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/domain_cfg_L31_TROPICO05.nc
+
+ln -sf ${parent_coord} $TMPDIR/coordinates.nc
+ln -sf ${parent_mesh} $TMPDIR/meshmask.nc
+ln -sf ${parent_bathy_meter} $TMPDIR/bathy_meter.nc
+ln -sf ${parent_domain} $TMPDIR/domain_cfg.nc
+
+
+#High resolution bathymetry
+
+bathy_hr=/mnt/meom/workdir/brodeau/data/bathy/Caledo_team/test_MNT.nc4
+name_hr=Depth
+
+ln -sf ${bathy_hr} $TMPDIR/bathy_hr.nc
+
+
+#NESTING TOOLS executable
+
+agrif_coord=/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/agrif_create_coordinates.exe
+agrif_bathy=/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/agrif_create_bathy.exe
+agrif_restart=/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/agrif_create_restart.exe
+
+
+#Namelist
+
+namelist=namelist_caledo10
+          
+cdffindij -w $lonmin $lonmax $latmin $latmax -c $parent_mesh > $TMPDIR/cdffindij-out.txt
+
+imin=$(tail -2 $TMPDIR/cdffindij-out.txt | head -1 | awk '{print $1}')
+imax=$(tail -2 $TMPDIR/cdffindij-out.txt | head -1 | awk '{print $2}')
+jmin=$(tail -2 $TMPDIR/cdffindij-out.txt | head -1 | awk '{print $3}')
+jmax=$(tail -2 $TMPDIR/cdffindij-out.txt | head -1 | awk '{print $4}')
+
+
+factor_space=5
+factor_time=5
+
+ppkth=21.43336197938
+ppacr=3.0
+ppdzmin=0 #999999. 
+pphmax=0 #999999. 
+psur=-4762.96143546300
+pa0=255.58049070440 
+pa1=245.58132232490
+N=31
+ldbletanh=.false.
+pa2=0 #999999.  
+ppkth2=0 #999999.  
+ppacr2=0 #999999.  
+
+cp /mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/tmp_namelist $TMPDIR/$namelist
+
+
+sed -i "s/NAMEHR/${name_hr}/g"           $TMPDIR/$namelist
+sed -i "s/IMIN/$imin/g"                  $TMPDIR/$namelist
+sed -i "s/IMAX/$imax/g"                  $TMPDIR/$namelist
+sed -i "s/JMIN/$jmin/g"                  $TMPDIR/$namelist
+sed -i "s/JMAX/$jmax/g"                  $TMPDIR/$namelist
+sed -i "s/RHOT/$factor_time/g"           $TMPDIR/$namelist
+sed -i "s/RHO/$factor_space/g"           $TMPDIR/$namelist
+sed -i "s/PPKTH2/$ppkth2/g"              $TMPDIR/$namelist
+sed -i "s/PPACR2/$ppacr2/g"              $TMPDIR/$namelist
+sed -i "s/PPKTH/$ppkth/g"                $TMPDIR/$namelist
+sed -i "s/PPACR/$ppacr/g"                $TMPDIR/$namelist
+sed -i "s/PPDZMIN/$ppdzmin/g"            $TMPDIR/$namelist
+sed -i "s/PPHMAX/$pphmax/g"              $TMPDIR/$namelist
+sed -i "s/PSUR/$psur/g"                  $TMPDIR/$namelist
+sed -i "s/PA0/$pa0/g"                    $TMPDIR/$namelist
+sed -i "s/PA1/$pa1/g"                    $TMPDIR/$namelist
+sed -i "s/NN/$N/g"                       $TMPDIR/$namelist
+sed -i "s/LDBLETANH/$ldbletanh/g"        $TMPDIR/$namelist
+sed -i "s/PA2/$pa2/g"                    $TMPDIR/$namelist
+
+#Running nesting tools
+
+cd TMPDIR
+
+$agrif_coord $namelist
+$agrif_bathy $namelist
--- nesting_tropico05-caledo10.py
+++ nesting_tropico05-caledo10.py
@@ -1,15 +1,49 @@
 #all the steps to create the nested files from TROPICO05 to CALEDO10
 
+# imports
+import os
+
 #NESTING TOOLS executable
 
-agrif_coord=
-agrif_bathy=
-agrif_restart=
+agrif_coord='/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/agrif_create_coordinates.exe'
+agrif_bathy='/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/agrif_create_bathy.exe'
+agrif_restart='/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/agrif_create_restart.exe'
 
 
 #TROPICO05 files
 
-parent_coord=
-parent_bathy=
+parent_coord='/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/coordinates_TROPICO05.nc'
+parent_mesh='/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/mesh_mask_TROPICO05_L31_r4.0.3.nc'
+parent_bathy_meter='/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/bathy_meter_TROPICO05.nc'
+parent_bathy_level='/mnt/meom/workdir/brodeau/TROPICO05/v1_TROPICO05-I/mesh_mask_TROPICO05_L31_r4.0.3.nc'
+
+#High resolution bathymetry
+
+bathy_hr='/mnt/meom/workdir/brodeau/data/bathy/Caledo_team/test_MNT.nc4'
+
+#Namelist
+
+namelist='/mnt/meom/workdir/alberta/NEMO/release-4.0/tools/NESTING/namelist_caledo10'
+
+#Nesting param
+latmin=-26.6
+latmax=-16.6
+lonmin=161.6
+lonmax=172
+        
+          
+os.system('cdffindij -w '+str(lonmin)+' '+str(lonmax)+' '+str(latmin)+ ' '+str(latmax)+' -c '+str(parent_mesh)+' > /mnt/meom/workdir/alberta/tmp/out.txt')
+if os.path.exists('/mnt/meom/workdir/alberta/tmp/out.txt'):
+	with open('/mnt/meom/workdir/alberta/tmp/out.txt','r') as txt_file:
+		last_line = txt_file.readlines()[-2]
+
+imin=os.system('tail -2 /mnt/meom/workdir/alberta/tmp/tmp.out | head -1 | awk "{print $1}" > /mnt/meom/workdir/alberta/tmp/')
+imax=
+jmin=
+jmax=
+rho=
+rhot=
+
+
 
 
